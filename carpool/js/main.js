0d5b0f9e16c3087213d3ab4224844b1dce4f0d02
Binh Nguyen <binh.nguyen@carpoolagency.com>
2015-10-26 20:29:35 -0700

    Carpool External Site build add Horizontal scrolling

--- carpool/js/main.js
+++ carpool/js/main.js
@@ -7,10 +7,270 @@
 */
 $(function(){
 
+    var currentChapterName = "home",
+    currentPageName = "welcome",
+    currentPageIndex = 0,
+    $currentChapterElm = null,
+    $visibleChapter = null,
+    transitionDurationTime = 800;
+    var lastScrollTop = 0;
+
+    // This represents the state of the object
+    var State = {
+        InitialLoad: false, // The page is initally loaded
+        InTransition: false, // The page is moving
+        Sleep: true // For other reason where we want to stop doing stuff
+    };
+
+    // This a list of selectors being used in this class.
+    // Not sure if this is the best way but it's probably better than hard coding.
+    var selectors = {
+        chapter: ".carpool-chapter",
+        horizontal: ".horizontal",
+        page: ".carpool-page",
+        scrollTarget: window,
+        navigation: ".carpool-nav"
+    };
+
+    var initChapter = function () {
+        $(currentChapterName).children(selectors.page).css({
+            height: 0,
+            overflow: 'hidden',
+            visibility: "hidden"
+        });
+    };
+
+    var activeNavAnchors = function (page) {
+        var $nav = $(selectors.navigation);
+        var selector1 = 'a[href="#' + page + '"]';
+        var $nav1 = $(selector1, $nav);
+        $("a", $nav).removeClass('active');
+        $nav1.addClass('active');
+
+        // Update the page title here since we can obtain human friendly page name from the link.
+        document.title = page;
+    };
+
+    // Get the top location of a page
+    var getPageOffset = function (page) {
+        var pageOffset = 0;
+        // Page offset is caculate by the offest of the page minus the header height
+        pageOffset = parseInt($($(page).get(0)).offset().top);
+        return pageOffset;
+    };
+
+    // A function for getting windows size
+    // This function automatically set the minimum width depending on the window size.
+    // If the window size is smaller than the body width, it sets the body width.
+    var getWindowSize = function () {
+        var size = {
+            width: 0,
+            height: 0,
+            changed: false
+        };
+        var oldOverflow = document.body.style.overflow;
+        document.body.style.overflow = "hidden";
+        size.originalHeight = $(window).outerHeight();
+        size.originalWidth = $(window).width();
+        document.body.style.overflow = oldOverflow;
+
+        //Height doesn't need to be changed
+        size.height = size.originalHeight;
+
+        //Get the body width to find the minimum width
+        var bodyWidth = $('body').width();
+
+        // check if the window size is smaller than the body width
+        if (size.originalWidth < bodyWidth) {
+            // Changed the width into the body width
+            size.width = bodyWidth;
+            size.changed = true;
+        } else {
+            // Use the original width
+            size.width = size.originalWidth;
+        }
+        return size;
+    };
+
+    var smoothScrollTo = function (pageOffset, options) {
+        if (Browser.isWP()) {
+            // In Window Phone, there is a default scroll animation so we just set the scroll top.
+            $(selectors.scrollTarget).scrollTop(pageOffset);
+            // Update the State
+            State.InTransition = false;
+        } else {
+            // Animate scroll to a page
+            $("html, body").animate({
+                scrollTop: pageOffset
+            }, options);
+        }
+    };
+
+    // Calculate the animation speed for hidding the header
+    var calculateSpeed = function (gap) {
+        var speed = 0;
+        if (gap < 1000) {
+            speed = 600;
+        } else if (gap < 2000) {
+            speed = 800;
+        } else if (gap < 3000) {
+            speed = 1200;
+        } else {
+            speed = gap / 2;
+        }
+        return speed;
+    };
+
+    // Show the targeted page
+    var showPage = function(chapter, page) {
+        // For updating the url
+        var showPageArguments = arguments;
+
+        // If page is null, it's the home for the chapter
+        // Set page to the chapter to make the transition works
+        if (page === null || page === "") {
+            page = chapter;
+            chapter = "home";
+            if (page === "home") {
+                page = "welcome";
+            }
+        }
+
+        // Ignore events that happen during the transition
+        if (State.InTransition === true) {
+            return;
+        }
+
+        // Get the current chapter and stored it in this object
+        $currentChapterElm = $('#' + chapter);
+
+        // Get the current page, if its a chapter then choose the first page
+        var $p = $('#' + page);
+
+        // If the page doesn't exist return
+        if ($p.length === 0) {
+            return;
+        }
+
+        // Get next page
+        var $nextPage = $p.next();
+
+        var scrollTop = $(selectors.scrollTarget).scrollTop();
+
+        // Recover the scroll top
+        $(window).scrollTop(scrollTop);
+
+        //Save page index
+        currentPageIndex = $currentChapterElm.children(selectors.page).index($p);
+
+        currentPageName = page;
+
+        // Set active nav
+        activeNavAnchors(page);
+
+        // Show the header if it is not welcome page
+        if (currentPageName !== "welcome") {
+            $(".header .logoContainer").slideDown(200);
+            $(".header .menuContainer").slideDown(200);
+        } else {
+            $(".header .logoContainer").slideUp(200);
+            $(".header .menuContainer").slideUp(200);
+        }
+
+        // Reset the last scroll top for navigation scrolling
+        lastScrollTop = 0;
+
+        // Get the windows size
+        var windowSize = getWindowSize();
+
+        // Position variables
+        var currentScrollTop = $(window).scrollTop();
+        var pageOffset = getPageOffset($p);
+        var nextPageOffset = ($nextPage.length !== 0) ? parseInt(getPageOffset($nextPage)) : 65535;
+        var self = this;
+        var finishTransition = function () {
+            State.InTransition = false;
+        };
+        var startHorizontalTransition = function () {
+            State.InTransition = true;
+            // If the showPageArguments length is more than 2,
+            // we don't scroll top. This is because it's most likely accessing an element within the chapter. (i.e. GPG)
+            if (!showPageArguments[2]) {
+                $(window).scrollTop(0);
+            }
+            $currentChapterElm.children(selectors.page).css({
+                height: "auto",
+                overflow: 'hidden',
+                visibility: "visible"
+            });
+        };
+        // For the horizontal layout, zero out the height of pages except the current one
+        // so that the window fits to the page height
+        var finishHorizontalTransition = function () {
+            $currentChapterElm.children(selectors.page).css({
+                height: 0,
+                overflow: 'hidden',
+                visibility: "hidden"
+            });
+            $p.css({
+                height: "inherit",
+                overflow: 'inherit',
+                visibility: "visible"
+            });
+            finishTransition();
+        };
+        // Horizontal variables
+        // TODO: We may need to add a support for older browsers (i.e. IE8) that don't support "vw".
+        var scrollingMargin = (currentPageIndex * $currentChapterElm.children(selectors.page).width() * -1);
+
+        // Immediately Scroll to initial page OR on chapter change
+        if (State.InitialLoad === true || ($visibleChapter && $visibleChapter.attr('id') !== $($currentChapterElm).attr('id'))) {
+            // Immediately scroll to chapter and page
+            if ($($currentChapterElm).hasClass('horizontal')) {
+                startHorizontalTransition();
+                $($currentChapterElm).css('margin-left', scrollingMargin);
+                finishHorizontalTransition();
+            } else {
+                $(selectors.scrollTarget).scrollTop(pageOffset);
+                finishTransition();
+            }
+            State.InitialLoad = false;
+        } else {
+            if ($($currentChapterElm).hasClass('horizontal')) {
+                startHorizontalTransition();
+                $($currentChapterElm).animate({
+                    'margin-left': scrollingMargin
+                }, {
+                    duration: transitionDurationTime,
+                    easing: 'swing',
+                    complete: function () {
+                        finishHorizontalTransition();
+                    }
+                });
+            } else {
+                // Check if the top of the page is below the scrolling position
+                // Check if the top of the next page is above the scrolling position
+                if (pageOffset > currentScrollTop || nextPageOffset <= currentScrollTop) {
+                    // Scroll when scrollTop is outside of the page
+                    State.InTransition = true;
+                    smoothScrollTo(pageOffset, {
+                        easing: 'swing',
+                        complete: finishTransition,
+                        duration: calculateSpeed(Math.abs(pageOffset - currentScrollTop))
+                    });
+                }
+            }
+        }
+    };
+
     // Smooth transition between pages
     $(".pageNavLink").unbind("click").bind("click", function(event) {
         event.preventDefault();
-        $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 1000);
+        //$('html,body').animate( { scrollTop:$(this.hash).offset().top } , 1000);
+        console.log("location: ", window.location);
+        console.log("PageNavLink: ", this);
+        var page = $(this).attr("href").replace("#", "");
+        showPage("home", page);
     });
 
     // Hide the navigation logo and menu on start up
@@ -26,22 +286,32 @@ $(function(){
     vid1.pause();
     vid2.pause();
 
-    var animationVideo1_offset = $("#welcome").height() / 2 - 100;
-    var animationVideo2_offset = $("#welcome").height() + $('#services').height() / 2 + 200;
+    var animationVideo1_offset = $("#welcome").height() / 10 - 100;
+    var animationVideo2_offset = $("#about").height() / 5 - 100;
+    // var animationVideo2_offset = $("#welcome").height() + $('#services').height() / 2 + 200;
 
     // Show the navigation on scrolling
     // Anchor offset with extra 100 height for unloaded image
     var anchor_offset = $("#welcome .video-overlay").offset().top + $("#welcome .video-overlay").height() + 100;
 
-    var lastScrollTop = 0;
+    
     $(window).scroll(function () {
+        console.log("currentPageName: ", currentPageName);
+        //$(".header .logoContainer").slideDown(200);
+        //$(".header .menuContainer").slideDown(200);
         var video1Time = (window.pageYOffset - animationVideo1_offset) / 200;
         var video2Time = (window.pageYOffset - animationVideo2_offset) / 200;
         // var videoTime = window.pageYOffset / 400;
-        // console.log("pageYOffset: ", window.pageYOffset);
-        // console.log("videoTime: ", videoTime);
-        vid1.currentTime = video1Time;
-        vid2.currentTime = video2Time;
+        console.log("pageYOffset: ", window.pageYOffset);
+        console.log("video1Time: ", video1Time);
+        console.log("video2Time: ", video2Time);
+        if (currentPageName === "welcome") {
+            vid1.currentTime = video1Time;
+        }
+
+        if (currentPageName === "about") {
+            vid2.currentTime = video2Time;
+        }
 
         // Detect scroll up or down
         var st = $(this).scrollTop();
@@ -49,15 +319,26 @@ $(function(){
         if (st > lastScrollTop){
             // Downscroll
             // Show navigation logo and hide navigation menu
-            if (st > anchor_offset) {
-                if ($(".header .logoContainer").is(':hidden')) {
-                    $(".header .logoContainer").slideDown(200);
-                }
+            if (currentPageName !== "welcome") {
                 if ($(".header .menuContainer").is(':visible')) {
                     $(".header .menuContainer").slideUp(200);
                 }
-                if ($(".downArrowContainer .downArrow").css("opacity") === "1") {
-                    $(".downArrowContainer .downArrow").fadeTo(100, 0.3);
+                if (st > anchor_offset) {
+                    if ($(".downArrowContainer .downArrow").css("opacity") === "1") {
+                        $(".downArrowContainer .downArrow").fadeTo(100, 0.3);
+                    }
+                }
+            } else {
+                if (st > anchor_offset) {
+                    if ($(".header .logoContainer").is(':hidden')) {
+                        $(".header .logoContainer").slideDown(200);
+                    }
+                    if ($(".header .menuContainer").is(':visible')) {
+                        $(".header .menuContainer").slideUp(200);
+                    }
+                    if ($(".downArrowContainer .downArrow").css("opacity") === "1") {
+                        $(".downArrowContainer .downArrow").fadeTo(100, 0.3);
+                    }
                 }
             }
         } else {
@@ -74,12 +355,22 @@ $(function(){
                     $(".downArrowContainer .downArrow").fadeTo(100, 0.3);
                 }
             } else { // Hide navigation logo and hide navigation menu if close to top
-                if ($(".header .logoContainer").is(':visible')) {
-                    $(".header .logoContainer").slideUp(200);
+                if (currentPageName === "welcome") {
+                    if ($(".header .logoContainer").is(':visible')) {
+                        $(".header .logoContainer").slideUp(200);
+                    }
+                    if ($(".header .menuContainer").is(':visible')) {
+                        $(".header .menuContainer").slideUp(200);
+                    }
+                } else {
+                    if ($(".header .logoContainer").is(':hidden')) {
+                        $(".header .logoContainer").slideDown(200);
+                    }
+                    if ($(".header .menuContainer").is(':hidden')) {
+                        $(".header .menuContainer").slideDown(200);
+                    }
                 }
-                if ($(".header .menuContainer").is(':visible')) {
-                    $(".header .menuContainer").slideUp(200);
-                }
+                
                 if ($(".downArrowContainer .downArrow").css("opacity") === "0.3") {
                     $(".downArrowContainer .downArrow").fadeTo(100, 1);
                 }
@@ -172,10 +463,13 @@ $(function(){
     //     }
     // });
 
+    // Hide all pages
+    initChapter();
 
-
-
-
-
-
+    // $("#home #welcome").css({
+    //     height: "auto",
+    //     overflow: 'hidden',
+    //     visibility: "visible"
+    // });
+    showPage("home", "welcome");
 });
